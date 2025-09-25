8b50df4c353d6c9d0564c7a811ad4e6cb2920cf1
Student Mykhailo Hnylytskyi (TL_KCOMP_D_Y3) <t00257938@mymtu.ie>
2025-09-25 10:50:46 +0100

    Added info here

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,18 @@
+/* GET 'home' page */
+const homelist = function(req, res){
+res.render('locations-list', { title: 'Home' });
+};
+/* GET 'Location info' page */
+const locationInfo = function(req, res){
+res.render('location-info', { title: 'Location info' });
+};
+
+/* GET 'Add review' page */
+const addReview = function(req, res){
+res.render('index', { title: 'Add review' });
+};
+module.exports = {
+homelist,
+locationInfo,
+addReview
+};
